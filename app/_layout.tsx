d085de29533374daa41aca4a2e1596599c0aba1d
Alkın Bayrak <alkin_bayrak@outlook.com>
2025-03-06 17:17:09 +0300

    auth işlemleri yapıldı clerkApi servisi eklendi

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,21 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
+import { tokenCache } from "@/cache";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  )
+}
 
 export default function RootLayout() {
   return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+
   <SafeAreaProvider>
     <SafeAreaView style={{flex: 1,backgroundColor:"#000"}}>
       <Stack screenOptions={{headerShown: false}}/>
@@ -10,5 +23,7 @@ export default function RootLayout() {
 
     </SafeAreaView>
   </SafeAreaProvider>
+  </ClerkLoaded>
+  </ClerkProvider>
   );
 };
